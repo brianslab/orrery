b90a5fc891727ac4795b4a1ca8cbb31032520928
brianslab <bdkuhn602@gmail.com>
2023-01-20 02:07:52 -0800

    update theta in bodies dict

--- orrery.py
+++ orrery.py
@@ -8,9 +8,9 @@ def orrery(days):
     print('After', days, 'Earth days, each celestial body will have orbited:')
 
     for body, value in bodies.items():
-        new_theta = round(to_seconds(1, 'd') /
-                          value['full_orbit']*360*days)
-        print(body, '-', new_theta, 'deg')
+        value['theta'] = round(to_seconds(1, 'd') /
+                               value['full_orbit']*360*days)
+        print(body, '-', value['theta'], 'deg')
 
 
-orrery(90)
+orrery(7)
