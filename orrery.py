a5607e4550295f83fd7d00d48e3fd7e02e5373ff
brianslab <bdkuhn602@gmail.com>
2023-01-20 02:04:12 -0800

    paramaterize rotations based on earth days

--- orrery.py
+++ orrery.py
@@ -3,8 +3,14 @@
 import utils
 from bodies import bodies
 
-print('After one Earth day, each celestial body will have orbited:')
 
-for body, value in bodies.items():
-    new_theta = utils.to_seconds(1, 'd')/value['full_orbit']*360
-    print(body, new_theta)
+def orrery(days):
+    print('After', days, 'Earth days, each celestial body will have orbited:')
+
+    for body, value in bodies.items():
+        new_theta = round(utils.to_seconds(1, 'd') /
+                          value['full_orbit']*360*days)
+        print(body, '-', new_theta, 'deg')
+
+
+orrery(90)
